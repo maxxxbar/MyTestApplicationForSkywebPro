4a372fa9ecd4c8d3374de911a8e2ccc1ea20eac4
Maxim <worldshine@yandex.ru>
2020-11-14 13:35:38 +0300

    Minor updates

--- app/src/main/java/com/worldshine/mytestapplicationforskywebpro/MainActivity.kt
+++ app/src/main/java/com/worldshine/mytestapplicationforskywebpro/MainActivity.kt
@@ -1,20 +1,11 @@
 package com.worldshine.mytestapplicationforskywebpro
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.LiveData
 import androidx.navigation.NavController
-import com.worldshine.mytestapplicationforskywebpro.data.PicturesDataSource
-import com.worldshine.mytestapplicationforskywebpro.data.PicturesRepository
 import com.worldshine.mytestapplicationforskywebpro.databinding.ActivityMainBinding
-import com.worldshine.mytestapplicationforskywebpro.network.Connection
-import com.worldshine.mytestapplicationforskywebpro.network.Rest
 import com.worldshine.mytestapplicationforskywebpro.utils.setupWithNavController
-import io.reactivex.android.schedulers.AndroidSchedulers
-import io.reactivex.rxkotlin.subscribeBy
-import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -28,31 +19,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(view)
         setNavigationBar()
 
-
-        testFun()
     }
 
-
-
-    private fun testFun() {
-/*        val retrofit = Connection.create
-        retrofit.getPictures(1)
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribeBy(
-                onError = {
-                    Log.d(TAG, "Error: ${it.localizedMessage}")
-                },
-                onSuccess = {
-                    Log.d(TAG, "Completed: ${it.joinToString()}")
-                }
-            )*/
-
-    val picturesRepository = PicturesRepository(Connection.getPicturesWithRetrofit())
-        picturesRepository.getResultAsLiveData().observe(this) {
-
-        }
-    }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
